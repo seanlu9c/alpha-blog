8aae69c51f45ec3275dff1f5c9b38f9fcaf25487
Sean <seanlu7@gmail.com>
2017-01-05 14:58:18 +0000

    Update current_user to be article creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,7 +33,8 @@ class ArticlesController < ApplicationController
     # render plain: params[:article].inspect
     
     @article = Article.new(article_params)
-    @article.user = User.first
+    # @article.user = User.first
+    @article.user = current_user
     # @article.save
     
     # # redirect_to articles_show(@article)   --WRONG
