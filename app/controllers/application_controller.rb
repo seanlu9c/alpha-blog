ae4a2e4f35454adea1d859106713e50ccbe27e35
Sean <seanlu7@gmail.com>
2017-01-05 14:04:04 +0000

    b4 117 restrict UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,33 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  
+  # default avail to all Controllers
+  # default not avail to VIEWS
+  
+  # HELPER_METHOD makes avail to VIEWS
+  helper_method :current_user, :logged_in?
+  
+  
+  
+  def current_user
+    # IF NOT "||="  then find ...
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    # convert to BOOLEAM using "bang,bang."
+    !!current_user
+    # if have then return true
+  end
+  
+  def require_user
+    if !logged_in?
+      flash[:danger] = "You must be logged in to perform that action"
+      redirect_to root_path
+    end
+    
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
   
   
   # lecture 107
@@ -24,11 +25,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
     if @user.update(user_params)
       
       flash[:success] = "Your account was updated successfully"
@@ -39,14 +40,20 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
     
   end
 
   private
+    
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+    
 
 end
