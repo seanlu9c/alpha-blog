ab91e22201adbd304c8e71e920e01675665a0b36
Sean <seanlu7@gmail.com>
2017-01-07 11:12:37 +0000

    Fix list categories test bug

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
   def index
     # @categories = Category.all
-    @categories = Category.paginate(page: params[:page], per_page: 5)
+    @categories = Category.paginate(page: params[:page], per_page: 5)  ## run rake test >> ensure code not broke
   end
   
   def new
